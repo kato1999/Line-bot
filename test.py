2cc3c64cd380294224a327f7d96dbb38c97b3359
kato1999 <kato.1999.cs@gmail.com>
2020-08-15 22:13:09 +0900

    test

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
-
 from flask import Flask, request, abort
 
 from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.exceptions import(
-    InvalidSignatureError, LineBotApiError
+    InvalidSignatureError
 )
 from linebot.models import(
-    MessageEvent, TextMessage, TextSendMessage, LocationMessage,
-    CarouselColumn, CarouselTemplate, FollowEvent,
-    TemplateSendMessage, UnfollowEvent, URITemplateAction
+    MessageEvent, TextMessage, TextSendMessage, LocationMessage
 )
 import os
 
@@ -19,11 +16,6 @@ from linebot.exceptions import LineBotApiError
 import requests
 from bs4 import BeautifulSoup
 import re
-
-import json
-import sys
-import urllib3.request
-# import urllib3.parse
 
 app = Flask(__name__)
 
@@ -34,99 +26,19 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-GNAVI_API_KEY = os.getenv('GNAVI_API_KEY')
-# BOT_SERVER_URL = os.getenv('BOT_SERVER_URL')
-# os.environ['http_proxy'] = os.getenv('FIXIE_URL')
-# os.environ['https_proxy'] = os.getenv('FIXIE_URL')
-
-if GNAVI_API_KEY is None:
-    print('Specify GNAVI_API_KEY as environment variable.')
-    sys.exit(1)
-if YOUR_CHANNEL_SECRET is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if YOUR_CHANNEL_ACCESS_TOKEN is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-# if BOT_SERVER_URL is None:
-#     print('Specify BOT_SERVER_URL as environment variable.')
-#     sys.exit(1)
-# if os.getenv('FIXIE_URL') is None:
-#     print('Specify FIXIE_URL as environment variable.')
-#     sys.exit(1)
-
-# instantiation
-# TODO: インスタンス生成はグローバルでなくファクトリメソッドに移したい
-# TODO: グローバルに参照可能な api_callerを作成するか, 個々に作成するかどちらが良いか確認
-app = Flask(__name__)
-line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-RESTSEARCH_URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
-DEF_ERR_MESSAGE = """
-申し訳ありません、データを取得できませんでした。
-少し時間を空けて、もう一度試してみてください。
-"""
-NO_HIT_ERR_MESSAGE = "お近くにぐるなびに登録されている喫茶店はないようです" + chr(0x100017)
-LINK_TEXT = "ぐるなびで見る"
-FOLLOWED_RESPONSE = "フォローありがとうございます。位置情報を送っていただくことで、お近くの喫茶店をお伝えします" + chr(0x100059)
-
-
-
-def call_restsearch(latitude, longitude):
-    
-    URL = "keyid=GNAVI_API_KEY&latitude=lt&longitude=lng&category_s=RSFST18008"
-    URL = URL.replace("GNAVI_API_KEY",GNAVI_API_KEY)
-    URL = URL.replace("lt",latitude)
-    URL = URL.replace("lng",longitude)
-
-    # params = urllib3.parse.urlencode(query, safe=",")
-    print(RESTSEARCH_URL+URL)
-    response = requests.get(RESTSEARCH_URL+URL)
-    result = json.loads(response)
-
-    if "error" in result:
-        if "message" in result:
-            raise Exception("{}".format(result["message"]))
-        else:
-            raise Exception(DEF_ERR_MESSAGE)
-
-    total_hit_count = result.get("total_hit_count", 0)
-    if total_hit_count < 1:
-        raise Exception(NO_HIT_ERR_MESSAGE)
-
-    return result
-
-
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
 
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info("Request body: "+body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    except LineBotApiError as e:
-        app.logger.exception(f'LineBotApiError: {e.status_code} {e.message}', e)
-        raise e
 
     return 'OK'
-
-    # signature = request.headers['X-Line-Signature']
-
-    # body = request.get_data(as_text=True)
-    # app.logger.info("Request body: "+body)
-
-    # try:
-    #     handler.handle(body, signature)
-    # except InvalidSignatureError:
-    #     abort(400)
-
-    # return 'OK'
 
 
 @handler.add(MessageEvent, message=TextMessage)
@@ -185,108 +97,19 @@ def handle_message(event):
           
             return result
         
-        # @handler.add(MessageEvent, message=LocationMessage)
-        # def handle_location(event):
-        #     if event.reply_token == "00000000000000000000000000000000":
-        #         return
-        #     text = event.message.address
+        @handler.add(MessageEvent, message=LocationMessage)
+        def handle_location(event):
+            if event.reply_token == "00000000000000000000000000000000":
+                return
+            text = event.message.address
         
-        #     result = get_weather_from_location(text)
-        #     line_bot_api.reply_message(
-        #         event.reply_token,
-        #         TextSendMessage(text=result)
-        #     )
-
-
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_location(event):
-    if event.reply_token == "00000000000000000000000000000000":
-        return
-
-    user_lat = event.message.latitude
-    user_longit = event.message.longitude
-
-    cafe_search_result = call_restsearch(user_lat, user_longit)
-    print("cafe_search_result is: {}".format(cafe_search_result))
-
-    response_json_list = []
-
-    # process result
-    for (count, rest) in enumerate(cafe_search_result.get("rest")):
-        # TODO: holiday, opentimeで表示を絞りたい
-        access = rest.get("access", {})
-        access_walk = "徒歩 {}分".format(access.get("walk", ""))
-        holiday = "定休日: {}".format(rest.get("holiday", ""))
-        image_url = rest.get("image_url", {})
-        # image1 = image_url.get("shop_image1", "thumbnail_template.jpg")
-        # if image1 == "":
-        #     image1 = BOT_SERVER_URL + "/static/thumbnail_template.jpg"
-        name = rest.get("name", "")
-        opentime = "営業時間: {}".format(rest.get("opentime", ""))
-        # pr = rest.get("pr", "")
-        # pr_short = pr.get("pr_short", "")
-        url = rest.get("url", "")
-
-        result_text = opentime + "\n" + holiday + "\n" + access_walk + "\n"
-        if len(result_text) > 60:
-            result_text = result_text[:56] + "..."
-
-        result_dict = {
-            "thumbnail_image_url": "https://1.bp.blogspot.com/--R3PJRI_b-w/XvcI5TatI2I/AAAAAAABZt4/LPy_kF8BYtgYy7R76I2C2N_hVWW6Bw2dwCNcBGAsYHQ/s1600/osyougatsu_text_2021.png",
-            "title": name,
-            # "text": pr_short + "\n" + opentime + "\n" + holiday + "\n"
-            # + access_walk + "\n",
-            "text": result_text,
-            "actions": {
-                "label": "ぐるなびで見る",
-                "uri": url
-            }
-        }
-        response_json_list.append(result_dict)
-    print("response_json_list is: {}".format(response_json_list))
-    columns = [
-        CarouselColumn(
-            thumbnail_image_url=column["thumbnail_image_url"],
-            title=column["title"],
-            text=column["text"],
-            actions=[
-                URITemplateAction(
-                    label=column["actions"]["label"],
-                    uri=column["actions"]["uri"],
-                )
-            ]
-        )
-        for column in response_json_list
-    ]
-    # TODO: GoogleMapへのリンク実装
-
-    messages = TemplateSendMessage(
-        alt_text="喫茶店の情報をお伝えしました",
-        template=CarouselTemplate(columns=columns),
-    )
-    print("messages is: {}".format(messages))
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        messages=messages
-    )
-
-@handler.add(FollowEvent)
-def handle_follow(event):
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=FOLLOWED_RESPONSE)
-    )
-
-
-@handler.add(UnfollowEvent)
-def handle_unfollow():
-    app.logger.info("Got Unfollow event")
-
+            result = get_weather_from_location(text)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=result)
+            )
 
 if __name__ == "__main__":
 #    app.run()
     port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
-
-
-
