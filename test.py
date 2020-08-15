856844ac8f6c875519e2c0ef02b2ad042037eb40
riri <ririho0112@gmail.com>
2020-08-15 21:36:11 +0900

    changed

--- test.py
+++ test.py
@@ -58,47 +58,9 @@ if YOUR_CHANNEL_ACCESS_TOKEN is None:
 # instantiation
 # TODO: インスタンス生成はグローバルでなくファクトリメソッドに移したい
 # TODO: グローバルに参照可能な api_callerを作成するか, 個々に作成するかどちらが良いか確認
-app = Flask(__name__)
+
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
-RESTSEARCH_URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
-DEF_ERR_MESSAGE = """
-申し訳ありません、データを取得できませんでした。
-少し時間を空けて、もう一度試してみてください。
-"""
-NO_HIT_ERR_MESSAGE = "お近くにぐるなびに登録されている喫茶店はないようです" + chr(0x100017)
-LINK_TEXT = "ぐるなびで見る"
-FOLLOWED_RESPONSE = "フォローありがとうございます。位置情報を送っていただくことで、お近くの喫茶店をお伝えします" + chr(0x100059)
-
-
-
-def call_restsearch(latitude, longitude):
-    query = {
-        "keyid": GNAVI_API_KEY,
-        "latitude": latitude,
-        "longitude": longitude,
-        # TODO: category_sを動的に生成
-        "category_s": "RSFST18008,RSFST18009,RSFST18010,RSFST18011,RSFST18012"
-        # TODO: hit_per_pageや offsetの変更に対応 (e.g., 指定可能にする, 多すぎるときは普通にブラウザに飛ばす, など)
-        # TODO: rangeをユーザーアクションによって選択可能にしたい
-        # "range": search_range
-    }
-    # params = urllib3.parse.urlencode(query, safe=",")
-    response = urllib3.request.urlopen(RESTSEARCH_URL + "?" + query["category_s"]).read()
-    result = json.loads(response)
-
-    if "error" in result:
-        if "message" in result:
-            raise Exception("{}".format(result["message"]))
-        else:
-            raise Exception(DEF_ERR_MESSAGE)
-
-    total_hit_count = result.get("total_hit_count", 0)
-    if total_hit_count < 1:
-        raise Exception(NO_HIT_ERR_MESSAGE)
-
-    return result
 
 
 @app.route("/callback", methods=['POST'])
@@ -132,12 +94,19 @@ def callback():
     # return 'OK'
 
 
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.reply_token == "00000000000000000000000000000000":
         return
         
     text = event.message.text
+
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=event.message.text)
+    # )
+
     if 'カフェ' in text:
         line_bot_api.reply_message(
             event.reply_token,
@@ -146,6 +115,235 @@ def handle_message(event):
             TextSendMessage(text='line://nv/location')
             ]
         )
+
+        RESTSEARCH_URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
+        DEF_ERR_MESSAGE = """
+        申し訳ありません、データを取得できませんでした。
+        少し時間を空けて、もう一度試してみてください。
+        """
+        NO_HIT_ERR_MESSAGE = "お近くにぐるなびに登録されている喫茶店はないようです" + chr(0x100017)
+        LINK_TEXT = "ぐるなびで見る"
+        FOLLOWED_RESPONSE = "フォローありがとうございます。位置情報を送っていただくことで、お近くの喫茶店をお伝えします" + chr(0x100059)
+
+
+        #-----------------call_restresearch------------------------------------------------
+        def call_restsearch(latitude, longitude):
+            query = {
+                "keyid": GNAVI_API_KEY,
+                "latitude": latitude,
+                "longitude": longitude,
+                # TODO: category_sを動的に生成
+                "category_s": "RSFST18008,RSFST18009,RSFST18010,RSFST18011,RSFST18012"
+                # TODO: hit_per_pageや offsetの変更に対応 (e.g., 指定可能にする, 多すぎるときは普通にブラウザに飛ばす, など)
+                # TODO: rangeをユーザーアクションによって選択可能にしたい
+                # "range": search_range
+            }
+            # params = urllib3.parse.urlencode(query, safe=",")
+
+            if "error" in result:
+                if "message" in result:
+                    raise Exception("{}".format(result["message"]))
+                else:
+                    raise Exception(DEF_ERR_MESSAGE)
+
+            total_hit_count = result.get("total_hit_count", 0)
+            if total_hit_count < 1:
+                raise Exception(NO_HIT_ERR_MESSAGE)
+
+            return result
+
+            #--------------------------------------------------------------------------------------
+
+            #-----------------handle_location_message----------------------------------------------
+
+            @handler.add(MessageEvent, message=LocationMessage)
+            def handle_location_message(event):
+                user_lat = event.message.latitude
+                user_longit = event.message.longitude
+
+                cafe_search_result = call_restsearch(user_lat, user_longit)
+                print("cafe_search_result is: {}".format(cafe_search_result))
+
+                response_json_list = []
+
+                # process result
+                for (count, rest) in enumerate(cafe_search_result.get("rest")):
+                    # TODO: holiday, opentimeで表示を絞りたい
+                    access = rest.get("access", {})
+                    access_walk = "徒歩 {}分".format(access.get("walk", ""))
+                    holiday = "定休日: {}".format(rest.get("holiday", ""))
+                    image_url = rest.get("image_url", {})
+                    image1 = image_url.get("shop_image1", "thumbnail_template.jpg")
+                    if image1 == "":
+                        image1 = BOT_SERVER_URL + "/static/thumbnail_template.jpg"
+                    name = rest.get("name", "")
+                    opentime = "営業時間: {}".format(rest.get("opentime", ""))
+                    # pr = rest.get("pr", "")
+                    # pr_short = pr.get("pr_short", "")
+                    url = rest.get("url", "")
+
+                    result_text = opentime + "\n" + holiday + "\n" + access_walk + "\n"
+                    if len(result_text) > 60:
+                        result_text = result_text[:56] + "..."
+
+                    result_dict = {
+                        "thumbnail_image_url": image1,
+                        "title": name,
+                        # "text": pr_short + "\n" + opentime + "\n" + holiday + "\n"
+                        # + access_walk + "\n",
+                        "text": result_text,
+                        "actions": {
+                            "label": "ぐるなびで見る",
+                            "uri": url
+                        }
+                    }
+                    response_json_list.append(result_dict)
+                print("response_json_list is: {}".format(response_json_list))
+                columns = [
+                    CarouselColumn(
+                        thumbnail_image_url=column["thumbnail_image_url"],
+                        title=column["title"],
+                        text=column["text"],
+                        actions=[
+                            URITemplateAction(
+                                label=column["actions"]["label"],
+                                uri=column["actions"]["uri"],
+                            )
+                        ]
+                    )
+                    for column in response_json_list
+                ]
+                # TODO: GoogleMapへのリンク実装
+
+                messages = TemplateSendMessage(
+                    alt_text="喫茶店の情報をお伝えしました",
+                    template=CarouselTemplate(columns=columns),
+                )
+                print("messages is: {}".format(messages))
+
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    messages=messages
+                )
+
+
+            @handler.add(FollowEvent)
+            def handle_follow(event):
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text=FOLLOWED_RESPONSE)
+                )
+
+
+            @handler.add(UnfollowEvent)
+            def handle_unfollow():
+                app.logger.info("Got Unfollow event")
+
+
+
+
+
+
+            #カフェスクレイピング
+            response = urllib3.request.urlopen(RESTSEARCH_URL + "?" + query["category_s"]).read()
+            result = json.loads(response)
+
+            if "error" in result:
+                if "message" in result:
+                    raise Exception("{}".format(result["message"]))
+                else:
+                    raise Exception(DEF_ERR_MESSAGE)
+
+            total_hit_count = result.get("total_hit_count", 0)
+            if total_hit_count < 1:
+                raise Exception(NO_HIT_ERR_MESSAGE)
+
+            return result
+
+            @handler.add(MessageEvent, message=LocationMessage)
+            def handle_location(event):
+                if event.reply_token == "00000000000000000000000000000000":
+                    return
+
+                user_lat = event.message.latitude
+                user_longit = event.message.longitude
+
+                cafe_search_result = call_restsearch(user_lat, user_longit)
+                print("cafe_search_result is: {}".format(cafe_search_result))
+
+                response_json_list = []
+
+                # process result
+                for (count, rest) in enumerate(cafe_search_result.get("rest")):
+                    # TODO: holiday, opentimeで表示を絞りたい
+                    access = rest.get("access", {})
+                    access_walk = "徒歩 {}分".format(access.get("walk", ""))
+                    holiday = "定休日: {}".format(rest.get("holiday", ""))
+                    image_url = rest.get("image_url", {})
+                    # image1 = image_url.get("shop_image1", "thumbnail_template.jpg")
+                    # if image1 == "":
+                    #     image1 = BOT_SERVER_URL + "/static/thumbnail_template.jpg"
+                    name = rest.get("name", "")
+                    opentime = "営業時間: {}".format(rest.get("opentime", ""))
+                    # pr = rest.get("pr", "")
+                    # pr_short = pr.get("pr_short", "")
+                    url = rest.get("url", "")
+
+                    result_text = opentime + "\n" + holiday + "\n" + access_walk + "\n"
+                    if len(result_text) > 60:
+                        result_text = result_text[:56] + "..."
+
+                    result_dict = {
+                        "thumbnail_image_url": "https://1.bp.blogspot.com/--R3PJRI_b-w/XvcI5TatI2I/AAAAAAABZt4/LPy_kF8BYtgYy7R76I2C2N_hVWW6Bw2dwCNcBGAsYHQ/s1600/osyougatsu_text_2021.png",
+                        "title": name,
+                        # "text": pr_short + "\n" + opentime + "\n" + holiday + "\n"
+                        # + access_walk + "\n",
+                        "text": result_text,
+                        "actions": {
+                            "label": "ぐるなびで見る",
+                            "uri": url
+                        }
+                    }
+                    response_json_list.append(result_dict)
+                print("response_json_list is: {}".format(response_json_list))
+                columns = [
+                    CarouselColumn(
+                        thumbnail_image_url=column["thumbnail_image_url"],
+                        title=column["title"],
+                        text=column["text"],
+                        actions=[
+                            URITemplateAction(
+                                label=column["actions"]["label"],
+                                uri=column["actions"]["uri"],
+                            )
+                        ]
+                    )
+                    for column in response_json_list
+                ]
+                # TODO: GoogleMapへのリンク実装
+
+                messages = TemplateSendMessage(
+                    alt_text="喫茶店の情報をお伝えしました",
+                    template=CarouselTemplate(columns=columns),
+                )
+                print("messages is: {}".format(messages))
+
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    messages=messages
+                )
+
+            @handler.add(FollowEvent)
+            def handle_follow(event):
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text=FOLLOWED_RESPONSE)
+                )
+
+
+            @handler.add(UnfollowEvent)
+            def handle_unfollow():
+                app.logger.info("Got Unfollow event")
+
+
     elif '天気' in text:
         line_bot_api.reply_message(
             event.reply_token,
@@ -200,90 +398,6 @@ def handle_message(event):
         #         TextSendMessage(text=result)
         #     )
 
-
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_location(event):
-    if event.reply_token == "00000000000000000000000000000000":
-        return
-
-    user_lat = event.message.latitude
-    user_longit = event.message.longitude
-
-    cafe_search_result = call_restsearch(user_lat, user_longit)
-    print("cafe_search_result is: {}".format(cafe_search_result))
-
-    response_json_list = []
-
-    # process result
-    for (count, rest) in enumerate(cafe_search_result.get("rest")):
-        # TODO: holiday, opentimeで表示を絞りたい
-        access = rest.get("access", {})
-        access_walk = "徒歩 {}分".format(access.get("walk", ""))
-        holiday = "定休日: {}".format(rest.get("holiday", ""))
-        image_url = rest.get("image_url", {})
-        # image1 = image_url.get("shop_image1", "thumbnail_template.jpg")
-        # if image1 == "":
-        #     image1 = BOT_SERVER_URL + "/static/thumbnail_template.jpg"
-        name = rest.get("name", "")
-        opentime = "営業時間: {}".format(rest.get("opentime", ""))
-        # pr = rest.get("pr", "")
-        # pr_short = pr.get("pr_short", "")
-        url = rest.get("url", "")
-
-        result_text = opentime + "\n" + holiday + "\n" + access_walk + "\n"
-        if len(result_text) > 60:
-            result_text = result_text[:56] + "..."
-
-        result_dict = {
-            "thumbnail_image_url": "https://1.bp.blogspot.com/--R3PJRI_b-w/XvcI5TatI2I/AAAAAAABZt4/LPy_kF8BYtgYy7R76I2C2N_hVWW6Bw2dwCNcBGAsYHQ/s1600/osyougatsu_text_2021.png",
-            "title": name,
-            # "text": pr_short + "\n" + opentime + "\n" + holiday + "\n"
-            # + access_walk + "\n",
-            "text": result_text,
-            "actions": {
-                "label": "ぐるなびで見る",
-                "uri": url
-            }
-        }
-        response_json_list.append(result_dict)
-    print("response_json_list is: {}".format(response_json_list))
-    columns = [
-        CarouselColumn(
-            thumbnail_image_url=column["thumbnail_image_url"],
-            title=column["title"],
-            text=column["text"],
-            actions=[
-                URITemplateAction(
-                    label=column["actions"]["label"],
-                    uri=column["actions"]["uri"],
-                )
-            ]
-        )
-        for column in response_json_list
-    ]
-    # TODO: GoogleMapへのリンク実装
-
-    messages = TemplateSendMessage(
-        alt_text="喫茶店の情報をお伝えしました",
-        template=CarouselTemplate(columns=columns),
-    )
-    print("messages is: {}".format(messages))
-
-    line_bot_api.reply_message(
-        event.reply_token,
-        messages=messages
-    )
-
-@handler.add(FollowEvent)
-def handle_follow(event):
-    line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=FOLLOWED_RESPONSE)
-    )
-
-
-@handler.add(UnfollowEvent)
-def handle_unfollow():
-    app.logger.info("Got Unfollow event")
 
 
 if __name__ == "__main__":
