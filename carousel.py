748ad98101553ff22b97e15cc3c954519428f361
riri <ririho0112@gmail.com>
2020-08-15 23:21:19 +0900

    changed

---
+++ carousel.py
@@ -0,0 +1,33 @@
+from linebot.models import (
+    CarouselTemplate, CarouselColumn,URITemplateAction)
+import requests
+
+def create_carousel(rest_colum):
+    carousel_template = CarouselTemplate(
+        columns=[
+            CarouselColumn(
+                text=rest["name"],
+                actions=[
+                    URITemplateAction(
+                        label="開く",
+                        uri=rest["url_mobile"]
+                        )]
+                    )
+                for rest in rest_colum
+                ])
+    return carousel_template
+
+def rest_search(lat,lon):
+    URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
+    api_params = {
+                "keyid":"ACCESS_KEY",
+                "category_s":"RSFST09004",
+                "latitude":lat,
+                "longitude":lon,
+                "range":3,
+                "hit_per_page":5
+                }
+
+    rest = requests.get(URL,params = api_params).json()["rest"]
+    json_datas = [data for data in rest]
+    return json_datas
