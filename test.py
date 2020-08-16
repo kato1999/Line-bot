92de1c0c7ac9e36454dbd0ecf12e1d17dd2d2f57
hirokiarai <hirokiarai@gmail.com>
2020-08-16 11:24:48 +0900

    commit

--- test.py
+++ test.py
@@ -89,12 +89,10 @@ def handle_message(event):
             lon = event.message.longitude
 
             rest_datas = rest_search(35.69,139.45)
-            # template_message = TextSendMessage(alt_text='周辺の居酒屋だよ!', template=create_carousel(rest_datas))
+            template_message = TemplateSendMessage(alt_text='周辺の居酒屋だよ!', template=create_carousel(rest_datas))
             line_bot_api.reply_message(
                 event.reply_token,
-                [
-                TextSendMessage(text=str(rest_datas))
-                ]
+                template_message
                 )
 
 
