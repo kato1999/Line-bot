b74eea0a038c916791f26349f9cadd488d163927
kato1999 <kato.1999.cs@gmail.com>
2020-08-16 10:15:32 +0900

    test

--- carousel.py
+++ carousel.py
@@ -20,8 +20,8 @@ def create_carousel(rest_colum):
 def rest_search(lat,lon):
     URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
     api_params = {
-                "keyid":"GNAVI_API_KEY",
-                # "category_s":"RSFST09004",
+                "keyid":"45747be45373a6faec585372092743d9",
+                "category_s":"RSFST09004",
                 "latitude":lat,
                 "longitude":lon,
                 "range":5,
